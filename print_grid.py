8c076905d6c6dc1cf0e0274c1d9374921aa93673
Quentin-Desmettre <quentin.desmettre@epitech.eu>
2022-02-03 15:37:36 +0100

    [+] Print grid

---
+++ print_grid.py
@@ -0,0 +1,33 @@
+#!/bin/python
+
+from random import *
+import os
+
+def putchr(c):
+    print("%c" %c, end="")
+
+def print_grid(grid):
+    os.system("clear")
+    l = len(grid)
+    print("    ", end="")
+    for i in range(l):
+        print("", chr(ord('A') + i), end="")
+    print("")
+    print("--", end="")
+    for i in range(l + 1):
+        print("--", end="")
+    print("")
+    for i in range(len(grid)):
+        if i < 9:
+            putchr(' ')
+        print(i + 1, "|", end=" ")
+        for j in range(len(grid[0])):
+            if (grid[i][j] == 0):
+                putchr('.')
+            if (grid[i][j] == -1):
+                putchr('X')
+            if (grid[i][j] == 1):
+                putchr('O')
+            print(" ", end ="")
+        print("")
+    print("")
