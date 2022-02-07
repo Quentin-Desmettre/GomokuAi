32ead1e03dae0edc378aadf9750245bd19c6eed7
EdgarLec <edgar.lecuyer@epitech.eu>
2022-02-07 11:39:05 +0100

    [+] columns verif

--- eval_pos.py
+++ eval_pos.py
@@ -5,12 +5,10 @@ def end_of_sequence(score, consecutive, open_ends, is_my_turn):
     consecutive = 0
     open_ends = 1
 
-def analyze_grid_for_color(grid, color, is_my_turn):
+def analyze_rows(grid, color, is_my_turn, score):
+    # analyze rows:
     consecutive = 0
     open_ends = 0
-    score = 0
-
-    # analyze rows:
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             # new valid char
@@ -34,6 +32,37 @@ def analyze_grid_for_color(grid, color, is_my_turn):
                 else:
                     open_ends = 0
 
+def analyze_columns(grid, color, is_my_turn, score):
+    # analyze rows:
+    consecutive = 0
+    open_ends = 0
+    for i in range(len(grid[0])):
+        for j in range(len(grid)):
+            # new valid char
+            if grid[i][j] == color:
+                consecutive += 1
+
+            # empty char
+            elif grid[i][j] == 0:
+                if consecutive > 0:
+                    # empty char and there is multiple valid char before
+                    open_ends += 1
+                    end_of_sequence(score, consecutive, open_ends, is_my_turn)
+                else:
+                    # empty char in a row
+                    open_ends = 1
+
+            # opponent char
+            else:
+                if consecutive > 0:
+                    end_of_sequence(score, consecutive, open_ends, is_my_turn)
+                else:
+                    open_ends = 0
+
+def analyze_grid_for_color(grid, color, is_my_turn):
+    score = 0
+    analyze_rows(grid, color, is_my_turn, score)
+    analyze_columns(grid, color, is_my_turn, score)
     pass
 
 def analyze_ia_pos(grid, is_ia_turn):
