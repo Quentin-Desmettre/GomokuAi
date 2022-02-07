12bd20c922b6a4bc8080e8cb09381a25e83d1aca
Quentin-Desmettre <quentin.desmettre@epitech.eu>
2022-02-07 17:55:57 +0100

    [~]Better diagonal detection

--- eval_pos.py
+++ eval_pos.py
@@ -37,46 +37,44 @@ def analyze_rows(grid, color, is_my_turn):
     return score
 
 def analyze_diag_right(grid, color, is_my_turn):
-    consecutive = 0
-    consecutive = 0
-    open_ends = 0
-    score = 0
-    for i in range(len(grid)):
-        k = 0
-        for j in range(len(grid)):
-            if k > len(grid) or j + i >= len(grid):
-                break
-            score, open_ends, consecutive = analyze_lines(k, i + j, score, open_ends, consecutive, grid, is_my_turn, color)
-            k += 1
-    for i in range(len(grid)):
-        k = 0
-        for j in range(len(grid)):
-            if k > len(grid) or j + i >= len(grid):
-                break
-            score, open_ends, consecutive = analyze_lines(i + j, k, score, open_ends, consecutive, grid, is_my_turn, color)
-            k += 1
-    return score
+    consecutive_bl = 0
+    score_bl = 0
+    nb_ends_bl = 0
+
+    consecutive_ur = 0
+    score_ur = 0
+    nb_ends_ur = 0
+
+    for i in range(11):
+        for j in range(len(grid) - i):
+            # bottom left
+            score_bl, nb_ends_bl, consecutive_bl =\
+                analyze_lines(i + j, j, score_bl, nb_ends_bl, consecutive_bl, grid, is_my_turn, color)
+            # up right
+            if j > 0 and i != 10:
+                score_ur, nb_ends_ur, consecutive_ur =\
+                    analyze_lines(j - 1, i + j, score_ur, nb_ends_ur, consecutive_ur, grid, is_my_turn, color)
+    return score_ur + score_bl
 
 def analyze_diag_left(grid, color, is_my_turn):
-    consecutive = 0
-    consecutive = 0
-    open_ends = 0
-    score = 0
-    for i in range(len(grid)):
-        k = 0
-        for j in range(len(grid)):
-            if k > len(grid) or j + i >= len(grid):
-                break
-            score, open_ends, consecutive = analyze_lines(k, 14 - (i + j), score, open_ends, consecutive, grid, is_my_turn, color)
-            k += 1
-    for i in range(len(grid)):
-        k = 0
-        for j in range(len(grid)):
-            if k > len(grid) or j + i >= len(grid):
-                break
-            score, open_ends, consecutive = analyze_lines(14 - (i + j), k, score, open_ends, consecutive, grid, is_my_turn, color)
-            k += 1
-    return score
+    consecutive_bl = 0
+    score_bl = 0
+    nb_ends_bl = 0
+
+    consecutive_ur = 0
+    score_ur = 0
+    nb_ends_ur = 0
+
+    for i in range(11):
+        for j in range(len(grid) - i):
+            # bottom left
+            score_bl, nb_ends_bl, consecutive_bl =\
+                analyze_lines(14 - (i + j), j, score_bl, nb_ends_bl, consecutive_bl, grid, is_my_turn, color)
+            # up right
+            if j > 0 and i != 10:
+                score_ur, nb_ends_ur, consecutive_ur =\
+                    analyze_lines(14 - (j - 1), i + j, score_ur, nb_ends_ur, consecutive_ur, grid, is_my_turn, color)
+    return score_ur + score_bl
 
 def analyze_columns(grid, color, is_my_turn):
     # analyze rows:
