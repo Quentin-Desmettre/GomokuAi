2f9f9bb7ac769ce6646be23b34a9e0a519062bcd
Quentin-Desmettre <quentin.desmettre@epitech.eu>
2022-02-08 15:48:02 +0100

    [+] Eval shape

--- cpp/gomoku.hpp
+++ cpp/gomoku.hpp
@@ -5,5 +5,6 @@ static const int SIZE = 19
 
 int who_won(char grid[19][19]);
 void print_winner(char grid[19][19]);
+unsigned eval_shape(unsigned count, unsigned open_ends, bool currentTurn);
 
 #endif
--- cpp/win_condition.cpp
+++ cpp/win_condition.cpp
@@ -1,5 +1,6 @@
 #include <utility>
 #include <iostream>
+#include "gomoku.hpp"
 
 void get_player_points(char cell_type, int &row_1, int &row_2)
 {
@@ -24,11 +25,11 @@ int player_win(int row_1, int row_2)
     return 0;
 }
 
-int is_line_win(char line[19])
+int is_line_win(char line[SIZE])
 {
     int row_1 = 0, row_2 = 0;
 
-    for (int i = 0; i < 19; i++) {
+    for (int i = 0; i < SIZE; i++) {
         get_player_points(line[i], row_1, row_2);
         if (row_1 >= 5 || row_2 >= 5)
             break;
@@ -36,12 +37,12 @@ int is_line_win(char line[19])
     return player_win(row_1, row_2);
 }
 
-int check_diag_right(char grid[19][19], int x, int y)
+int check_diag_right(char grid[SIZE][SIZE], int x, int y)
 {
     char ch = grid[x][y];
 
     for (int i = 0; i < 5; i++) {
-        if (x + i >= 19 || y + i >= 19)
+        if (x + i >= SIZE || y + i >= SIZE)
             return 0;
         if (grid[x + i][y + i] != ch)
             return 0;
@@ -49,12 +50,12 @@ int check_diag_right(char grid[19][19], int x, int y)
     return ch;
 }
 
-int check_diag_left(char grid[19][19], int x, int y)
+int check_diag_left(char grid[SIZE][SIZE], int x, int y)
 {
     char ch = grid[x][y];
 
     for (int i = 0; i < 5; i++) {
-        if (x + i >= 19 || y - i < 0)
+        if (x + i >= SIZE || y - i < 0)
             return 0;
         if (grid[x + i][y - i] != ch)
             return 0;
@@ -62,19 +63,19 @@ int check_diag_left(char grid[19][19], int x, int y)
     return ch;
 }
 
-int is_diag_win(char grid[19][19], int x, int y)
+int is_diag_win(char grid[SIZE][SIZE], int x, int y)
 {
     int c = check_diag_right(grid, x, y);
 
     return (c != 0) ? c : check_diag_left(grid, x, y);
 }
 
-int is_column_win(char grid[19][19], int column)
+int is_column_win(char grid[SIZE][SIZE], int column)
 {
     int row_1 = 0;
     int row_2 = 0;
     
-    for (int i = 0; i < 19; i++) {
+    for (int i = 0; i < SIZE; i++) {
         get_player_points(grid[i][column], row_1, row_2);
         if (row_1 >= 5 || row_2 >= 5)
             break;
@@ -82,11 +83,11 @@ int is_column_win(char grid[19][19], int column)
     return player_win(row_1, row_2);
 }
 
-int who_won(char grid[19][19])
+int who_won(char grid[SIZE][SIZE])
 {
     int win = 0;
 
-    for (int i = 0; i < 19; i++) {
+    for (int i = 0; i < SIZE; i++) {
         win = is_line_win(grid[i]);
         if (win != 0)
             return win;
@@ -94,8 +95,8 @@ int who_won(char grid[19][19])
         if (win != 0)
             return win;
     }
-    for (int i = 0; i < 19; i++)
-        for (int j = 0; j < 19; j++) {
+    for (int i = 0; i < SIZE; i++)
+        for (int j = 0; j < SIZE; j++) {
             win = is_diag_win(grid, i, j);
             if (win != 0)
                 return win;
@@ -103,7 +104,7 @@ int who_won(char grid[19][19])
     return 0;
 }
 
-void print_winner(char grid[19][19])
+void print_winner(char grid[SIZE][SIZE])
 {
     int w = who_won(grid);
 
