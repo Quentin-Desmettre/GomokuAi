1820b7e5b84e2352670029a49a3bc78fdcaaee11
EdgarLec <edgar.lecuyer@epitech.eu>
2022-02-08 19:14:35 +0100

    [*] correc print grid

--- cpp/print_grid.cpp
+++ cpp/print_grid.cpp
@@ -12,20 +12,20 @@ void clear() {
 
 void print_grid(char grid[SIZE][SIZE])
 {
-    clear();
+    // clear();
     int l = SIZE;
-    std::cout << "    ";
-    for (int i; i < l; i++)
-        std::cout << 'A' + i;
+    std::cout << "   ";
+    for (int i = 0; i < l; i++)
+        std::cout << char('A' + i) << " ";
     std::cout << "\n--";
-    for (int i; i < l; i++)
+    for (int i = 0; i < l; i++)
         std::cout <<"--";
     std::cout << "\n";
-    for (int i; i < SIZE; i++) {
+    for (int i = 0; i < SIZE; i++) {
         if (i < 9)
             std::cout << " ";
         std::cout << i + 1 << "|";
-        for (int j; j < SIZE; j++) {
+        for (int j = 0; j < SIZE; j++) {
             if (grid[i][j] == 0)
                 std::cout << '.';
             if (grid[i][j] == -1)
